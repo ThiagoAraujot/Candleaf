ab0edb086f776dce193d5dfaa308521bdb2dc4a0
Thiago Araujo <131808175+ThiagoAraujot@users.noreply.github.com>
2023-11-06 15:25:13 -0300

    feat keyframes and index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+function aplicarAnimacaoNaSecao(secaoId, animacaoClass) {
+    const secaoParaAnimar = document.getElementById(secaoId);
+    if (!secaoParaAnimar) return; 
+    
+    const secaoParaAnimarPosicao = secaoParaAnimar.getBoundingClientRect();
+
+    if (secaoParaAnimarPosicao.top < window.innerHeight && secaoParaAnimarPosicao.bottom >= 0) {
+        secaoParaAnimar.classList.add(animacaoClass);
+    } else {
+        secaoParaAnimar.classList.remove(animacaoClass);
+    }
+}
+
+window.addEventListener('scroll', function () {
+    aplicarAnimacaoNaSecao('section-products', 'fadeIn');
+    aplicarAnimacaoNaSecao('benefits', 'fadeLeft');
+    aplicarAnimacaoNaSecao('testimonials', 'fadeIn');
+});
